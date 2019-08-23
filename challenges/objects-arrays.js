9842d43ed7f0cb8957e9d4f9962f80fe7f54d2a7
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-08-23 15:26:30 -0400

    Update objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -92,8 +92,10 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
 graduates.forEach(function(element){
-  let uni = element.university.includes("Uni");
-  unisWithUni.push(uni);     
+  let uni = element.university.includes("Uni"); 
+  if (uni) {
+    unisWithUni.push(element);
+  }    
   });
 console.log(unisWithUni);
 
